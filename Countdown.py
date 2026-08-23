ca0e55392a39609a3c6a2b169b97bcd0c65b92f2
yancat <frz114514@icloud.com>
2026-08-23 17:34:11 +0800

    构建工作流

---
+++ Countdown.py
@@ -0,0 +1,29 @@
+import info
+from Package.CMakePackageBase import *
+from CraftCore import CraftCore
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.displayName = "Countdown"
+        self.description = "A countdown application built with Kirigami"
+        self.webpage = "https://github.com/yan-cat/countdown"
+
+        self.svnTargets["main"] = "[git]https://github.com/yan-cat/countdown.git|main"
+        self.defaultTarget = "main"
+
+    def setDependencies(self):
+        # 构建依赖
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+
+        # 运行时依赖
+        self.runtimeDependencies["libs/qt6/qtbase"] = None
+        self.runtimeDependencies["libs/qt6/qtdeclarative"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
